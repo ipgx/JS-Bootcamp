d172fca92bce8a2c5894af1b3880f05e8b0bcea4
ipgx <parag_x_gupta@proton.me>
2026-01-03 17:54:36 -0500

    Learning Arrays in JS

---
+++ Arrays/00_Arrays.js
@@ -0,0 +1,10 @@
+let arr = [10, 12, 15, 21, 25];
+console.log(arr);
+
+let fruits = ["Kiwi", "Orange", "Apple", "Mango"];
+console.log(fruits);
+console.log("Length of fruits is :", fruits.length);
+
+// Array can store multiple data types
+const mixedArr = [11, true, NaN, null, "Hello"];
+console.log(mixedArr);
